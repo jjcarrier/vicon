bb6cbfe6b96240e30acd45e0a3aaf77e4f7cc593
Jon Carrier <jjcarrier@gmail.com>
2025-02-22 11:34:47 -0500

    Added serial-number tab-completion
    Serial numbers are now tab completed in the CLI via the
    VIConCompletion.psm1 module.

--- completion/VIConCompletion.psm1
+++ completion/VIConCompletion.psm1
@@ -1,3 +1,8 @@
+function Get-PsuSerialNumbers()
+{
+    $devices = vicon --enumerate --json | ConvertFrom-Json
+    $devices | ForEach-Object { $_.Response.Device.SerialNumber }
+}
 
 $viconScriptBlock = {
     param($wordToComplete, $commandAst, $cursorPosition)
@@ -11,15 +16,28 @@ $viconScriptBlock = {
         Get-ParsedHelpFlag -HelpData $helpData |
             New-ParsedHelpParamCompletionResult -WordToComplete $wordToComplete
     } else {
-        $resultPrefix = ''
-        $values = $helpData |
-            Get-ParsedHelpParamValue `
-                -WordToComplete $wordToComplete `
-                -CommandAst $commandAst `
-                -CursorPosition $cursorPosition `
-                -ParamValueAssignment:$paramValueAssign `
-                -ResultPrefix ([ref]$resultPrefix)
-        $values | New-ParsedHelpValueCompletionResult -ResultPrefix $resultPrefix
+
+        # If the previous option flag is "--serial" or "--sn" then create completion results
+        # for each item returned in Get-PsuSerialNumbers().
+        $previousFlag = $commandAst.CommandElements |
+            Where-Object { $_.Extent.EndOffset -lt $cursorPosition } |
+            Select-Object -Last 1
+
+        if ($previousFlag -and ($previousFlag -match '--serial|--sn')) {
+            return Get-PsuSerialNumbers | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
+        } else {
+            $resultPrefix = ''
+            $values = $helpData |
+                Get-ParsedHelpParamValue `
+                    -WordToComplete $wordToComplete `
+                    -CommandAst $commandAst `
+                    -CursorPosition $cursorPosition `
+                    -ParamValueAssignment:$paramValueAssign `
+                    -ResultPrefix ([ref]$resultPrefix)
+            $values | New-ParsedHelpValueCompletionResult -ResultPrefix $resultPrefix
+        }
     }
 }
 
