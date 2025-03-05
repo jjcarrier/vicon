cc9b44514409efc2c2e3ea0c05f13029522c6bc8
Jon Carrier <jjcarrier@gmail.com>
2025-03-04 21:45:52 -0500

    Added theme completion
    Also minor improvement to serial number handling.

--- completion/VIConCompletion.psm1
+++ completion/VIConCompletion.psm1
@@ -24,7 +24,12 @@ $viconScriptBlock = {
             Select-Object -Last 1
 
         if ($previousFlag -and ($previousFlag -match '--serial|--sn')) {
-            return Get-PsuSerialNumbers | ForEach-Object {
+            Get-PsuSerialNumbers | Where-Object { $_ -like "*$wordToComplete*" } | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
+        } elseif ($previousFlag -and ($previousFlag -match '--theme')) {
+            @('classic', 'black-and-white', 'grey', 'dark-red', 'dark-green', 'dark-magenta', 'cyan', 'gold', 'blue', 'blue-violet') |
+                Where-Object { $_ -like "*$wordToComplete*" } | ForEach-Object {
                 [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
             }
         } else {
