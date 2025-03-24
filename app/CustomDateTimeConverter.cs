aa96ee28bf355cba2a1f606c75356c73c1a2d218
Jon Carrier <jjcarrier@gmail.com>
2025-03-23 21:10:21 -0400

    Changed namespace
    Updated to match rest of application.

--- app/CustomDateTimeConverter.cs
+++ app/CustomDateTimeConverter.cs
@@ -2,7 +2,7 @@ using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace PowerControllerApp
+namespace PowerSupplyApp
 {
     public class CustomDateTimeConverter : JsonConverter<DateTime>
     {
