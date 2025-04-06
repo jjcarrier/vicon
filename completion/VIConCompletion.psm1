fc05b4eb0167f0fec7120cd1335522534da17a84
Jon Carrier <jjcarrier@gmail.com>
2025-04-06 15:12:41 -0400

    Updated tab-completer
    Updated tooltip to show alias.

--- completion/VIConCompletion.psm1
+++ completion/VIConCompletion.psm1
@@ -1,7 +1,17 @@
-function Get-PsuSerialNumbers()
+function Get-PsuDevices()
 {
     $devices = vicon --enumerate --json | ConvertFrom-Json
-    $devices | ForEach-Object { $_.Response.Device.SerialNumber }
+    $devices | ForEach-Object {
+        $dev = [PSCustomObject]@{
+            Alias = $_.Response.Device.Alias
+            Serial = $_.Response.Device.SerialNumber
+        }
+        if ([string]::IsNullOrWhiteSpace($dev.Alias)) {
+            $dev.Alias = $dev.Serial
+        }
+
+        $dev
+    }
 }
 
 $viconScriptBlock = {
@@ -18,14 +28,18 @@ $viconScriptBlock = {
     } else {
 
         # If the previous option flag is "--serial" or "--sn" then create completion results
-        # for each item returned in Get-PsuSerialNumbers().
+        # for each item returned in Get-PsuDevices().
         $previousFlag = $commandAst.CommandElements |
             Where-Object { $_.Extent.EndOffset -lt $cursorPosition } |
             Select-Object -Last 1
 
         if ($previousFlag -and ($previousFlag -match '--serial|--sn')) {
-            Get-PsuSerialNumbers | Where-Object { $_ -like "*$wordToComplete*" } | ForEach-Object {
-                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            Get-PsuDevices | Where-Object { $_.Serial -like "*$wordToComplete*" } | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new(
+                    $_.Serial,
+                    $_.Serial,
+                    'ParameterValue',
+                    $_.Alias)
             }
         } elseif ($previousFlag -and ($previousFlag -match '--theme')) {
             @('classic', 'black-and-white', 'grey', 'dark-red', 'dark-green', 'dark-magenta', 'cyan', 'gold', 'blue', 'blue-violet') |
